ce96ece2dc355cdc0e0f6d91804377e9874ce435
Tyson Phalp <tyson@getweave.com>
2015-05-06 08:47:59 -0600

    Tweaking stuff, getting scss in place

---
+++ web/app/scripts/components/shared/MasterMenu.jsx
@@ -0,0 +1,13 @@
+var React = require('react/addons');
+
+var MasterMenu = React.createClass({
+  render: function() {
+    return (
+      <div className="MasterMenu">
+        <i className="fa fa-bars"></i>
+      </div>
+    );
+  }
+});
+
+module.exports = MasterMenu;
